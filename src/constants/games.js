8a4d648130519ba79eefa6cda37222afc1c3de4b
BSHAW63 <bshaw@levi.com>
2026-08-06 06:39:20 +0530

    chore: remove Ludo from main page games list

--- src/constants/games.js
+++ src/constants/games.js
@@ -34,11 +34,4 @@ export const GAMES = [
     description: "Solve math problems quickly",
     color: "#2ecc71",
   },
-  {
-    name: "Ludo Classic",
-    icon: "🎲",
-    path: "/ludo",
-    description: "Classic dice & token strategy game",
-    color: "#ff6b6b",
-  },
 ];
